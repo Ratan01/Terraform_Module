709332168969de0c0ec885a2724539c539d2e4bb
Ratan01 <ratanrudra010@gmail.com>
2023-09-13 13:07:11 +0530

    resource name updated

--- module/key_vault/main.tf
+++ module/key_vault/main.tf
@@ -2,15 +2,15 @@ data "azurerm_client_config" "current" {
 
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
 resource "azurerm_key_vault" "kva" {
   name                        = var.vault_name
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
+  location                    = azurerm_resource_group.rg1.location
+  resource_group_name         = azurerm_resource_group.rg1.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
--- module/key_vault/output.tf
+++ module/key_vault/output.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg1.name
 }
 
 output "azurerm_key_vault_name" {
