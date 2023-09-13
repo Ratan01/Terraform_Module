8499489252655bbef38e2d3c59ce5f33399ec3e0
Ratan01 <ratanrudra010@gmail.com>
2023-09-13 20:51:23 +0530

    file updated

---
+++ main.tf
@@ -0,0 +1,18 @@
+#####  for key vault module 
+module "key_vault" {
+  source = "./module/key_vault" 
+  resource_group_name        = "rg-keyvault-resource"
+  resource_group_location    = "east us"
+  key_vault_name             = "keyvault-grkk"
+  key_vault_sku_pricing_tier = "standard"
+}
+
+
+
+##### for storage module
+module "storage_account" {
+  source = "./module/storage_account"
+  resource_group_name        = "rg-storageacc-resource"
+  resource_group_location    = "east us"
+  storage_account_name       = "storage-rktgff"
+}
