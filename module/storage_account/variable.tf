502cc7933ff41aed1e76ca12e347df4fe9710d70
Ratan01 <ratanrudra010@gmail.com>
2023-09-13 12:56:29 +0530

    file added

---
+++ module/storage_account/variable.tf
@@ -0,0 +1,14 @@
+variable "azurerm_storage_account" {
+    type        = string
+    description = "storage account name."
+}
+
+variable "resource_group_name" {
+    type        = string
+    description = "name for resource group."
+}
+
+variable "resource_group_location" {
+    type =string
+    description = "location for resource group."
+}
