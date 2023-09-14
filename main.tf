0ccd49609c43fae9cb77464758b5ff9cedd6af32
Ratan01 <ratanrudra010@gmail.com>
2023-09-14 12:35:23 +0530

    names are updated

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 #####  for key vault module 
 module "key_vault" {
   source = "./modules/key_vault" 
-  resource_group_name        = "rg-keyvault-resource"
+  resource_group_name        = "keyvaultresourcerkok"
   resource_group_location    = "east us"
-  key_vault_name             = "keyvault-grkk"
+  key_vault_name             = "keyvaultgrkk"
   #key_vault_sku_pricing_tier = "standard"
 }
 
@@ -12,7 +12,7 @@ module "key_vault" {
 ##### for storage module
 module "storage_account" {
   source = "./modules/storage_account"
-  resource_group_name        = "rg-storageacc-resource"
+  resource_group_name        = "storageaccresourcerk"
   resource_group_location    = "east us"
   azurerm_storage_account       = "storagerktgff"
 }
